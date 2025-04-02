e229da9732f467978f51143f83736ea344faaa1a
Faisal ck <faisal@buhaleeba.ae>
2025-04-02 16:53:11 +0400

    commit on 2025 april 1st 7

--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -74,7 +74,7 @@
                                 Contract
                             </button>
                             <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <li><a class="dropdown-item" href="{{ route('cars-taxi') }}"  wire:click="cartaxiClick('unique_id1')" wire:navigate>Dubai Car Taxi
+                                <li><a class="dropdown-item" href="{{ route('cars-taxi') }}"  wire:click="cartaxiClick('unique_id1')" wire:navigate>KABIL L.L.C
                                     <span wire:target="cartaxiClick('unique_id1')" wire:loading> {{-- new wire:navigating directive --}}
                                         <div style="display: flex; justify-content: center; align-items: center; background-color: black; position: fixed; top: 0px; left: 0px; z-index:999999; width:100%; height:100%; opacity: .75;" >
                                             <div class="la-ball-beat">
