54dd3f5b7a1d79f9ec4fc6104404d5d8955a88d1
Faisal ck <faisal@buhaleeba.ae>
2025-04-12 14:53:11 +0400

    commit on 2025 april 12th 2

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="noindex, nofollow" />
     <meta name="autocomplete" content="off">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Metas -->
     @if(env('IS_DEMO'))
         <x-demo-metas></x-demo-metas>
