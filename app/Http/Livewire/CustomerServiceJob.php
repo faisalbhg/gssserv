e911e8342989ad286ba449dc814764325e42f52c
Faisal ck <faisal@buhaleeba.ae>
2025-04-15 09:41:46 +0400

    commit on 2025 april 15th 2

--- app/Http/Livewire/CustomerServiceJob.php
+++ app/Http/Livewire/CustomerServiceJob.php
@@ -95,7 +95,7 @@ class CustomerServiceJob extends Component
 
     public function render()
     {
-        dd(CustomerJobCards::limit(1)->get());
+        //dd(CustomerJobCards::limit(1)->get());
         if($this->new_make)
         {
             $this->makeSearchResult = VehicleMakes::where('vehicle_name','like',"%{$this->new_make}%")->where('is_deleted','=',null)->get();
